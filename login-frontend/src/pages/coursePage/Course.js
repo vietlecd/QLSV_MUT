7e178cca47b615a261aaad5efd3ccc6761d38bfb
nhattpn <nhat.trantpn2212@hcmut.edu.vn>
2024-04-23 08:13:52 +0700

    update to fix thienle Branch 1

--- login-frontend/src/pages/coursePage/Course.js
+++ login-frontend/src/pages/coursePage/Course.js
@@ -7,16 +7,16 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Button } from 'react-bootstrap';
 
-import Header from './../header_footer/Header';
-import Footer from './../header_footer/Footer';
+import StudentHeader from '../../components/header_footer/StudentHeader';
+import Footer from '../../components/header_footer/Footer';
 
 function Course() {
   const course_bg = 'http://i.ibb.co/GVRnWJC/course-bg.jpg';
   
   return (
     <>
-      <Header />
-      <div style={{ height: '80vh', backgroundImage: `url(${course_bg})`, backgroundSize: 'cover', backgroundPositionX: 'center', backgroundRepeat: 'noRepeat', border: '1px solid' }}>
+      <StudentHeader />
+      <div style={{ height: '80vh', backgroundImage: `url(${course_bg})`, backgroundSize: 'cover', backgroundPositionX: 'center', backgroundRepeat: 'noRepeat' }}>
         <div style={{ marginTop: '0%', marginRight: 'auto', marginBottom: '0%', marginLeft: '15%', width: '50%', paddingTop: '2%', border: 'none' }}>
           <h1>CHĂM CHỈ DỄ DÀNG THÀNH CÔNG</h1>
         </div>
