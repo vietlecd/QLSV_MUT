bdab22fe6fd9a06a211913671b00d9d2db675546
lagomst <133565804+lagomst@users.noreply.github.com>
2024-05-04 17:52:08 +0700

    Add files via upload

---
+++ qlsv_be/configs/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+          
+cloudinary.config({ 
+  cloud_name: 'dw1ukjibi', 
+  api_key: '195979345815261', 
+  api_secret: 'vCD7UFyWuj_tGKXy9Z2u1uQCL_4' 
+});
+
+module.exports = cloudinary
